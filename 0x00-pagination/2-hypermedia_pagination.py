5a3a1b6cf096b6df5d1a352a83622eeef2e3688c
amineNouabi <aminenouabi9@gmail.com>
2024-11-01 17:19:19 +0100

    hypermedia_pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 """
-Module for Simple pagination
+Module for Hypermedia pagination
 
 """
 import csv
+from math import ceil
 from typing import List
 
 
@@ -39,3 +40,15 @@ class Server:
         assert page > 0 and page_size > 0
         left, right = index_range(page, page_size)
         return self.dataset()[left:right]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> object:
+        """"""
+        total_pages = ceil(len(self.dataset()) / page_size)
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if page + 1 <= total_pages else None,
+            "prev_page": page - 1 if page - 1 > 0 else None,
+            "total_pages": total_pages
+        }
